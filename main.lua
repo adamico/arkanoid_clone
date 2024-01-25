722ef33bef178348644f2b5cac5584e5d79f42c4
Andrea D'Amico <andrea.damico@gmail.com>
2024-01-25 19:28:52 +0100

    collisions with box2d

--- main.lua
+++ main.lua
@@ -1,209 +1,181 @@
+local screen_width = love.graphics.getWidth()
+local screen_height = love.graphics.getHeight()
+
 -- bricks
 local bricks = {}
 bricks.width, bricks.height = 50, 30
-bricks.rows, bricks.columns = 8, 11
-bricks.top_left_position = {x = 70, y = 50}
+bricks.rows, bricks.columns = 7, 11
+bricks.top_left_position = {x = 70, y = 70}
 bricks.gap = {width = 10, height = 10}
-bricks.current_level_bricks = {}
 
 function bricks.construct_level()
+    Bricks = {}
+    local brick_index = ""
     for row = 1, bricks.rows do
         for col = 1, bricks.columns do
             local new_brick_position = {
                 x = bricks.top_left_position.x +
-                (col - 1) *
-                (bricks.width + bricks.gap.width),
+                    bricks.width/2 +
+                    (col - 1) *
+                    (bricks.width + bricks.gap.width),
                 y = bricks.top_left_position.y +
-                (row - 1) *
-                (bricks.height + bricks.gap.height)
+                    bricks.height/2 +
+                    (row - 1) *
+                    (bricks.height + bricks.gap.height)
             }
-            local new_brick = bricks.add_brick(new_brick_position)
+            brick_index = row..col
+            bricks.add_brick(brick_index, new_brick_position)
         end
     end
 end
 
-function bricks.add_brick(position)
-    local brick = {}
-    brick.position = position
-    brick.width = bricks.width
-    brick.height = bricks.height
-    table.insert(bricks.current_level_bricks, brick)
+function bricks.add_brick(index, position)
+    print(index)
+    local brick_object = {
+        body = love.physics.newBody(World, position.x, position.y, 'static'),
+        shape = love.physics.newRectangleShape(bricks.width, bricks.height)
+    }
+    brick_object.fixture = love.physics.newFixture(brick_object.body, brick_object.shape)
+    brick_object.fixture:setUserData({"Brick", index})
+    Bricks[index] = brick_object
 end
 
 
 function bricks.draw()
-    for _, brick in pairs(bricks.current_level_bricks) do
-        Draw_object_rectangle(brick)
+    for _, brick in pairs(Bricks) do
+        love.graphics.polygon("line", brick.body:getWorldPoints(brick.shape:getPoints()))
     end
-end
-
-function bricks.update(dt)
-    for _, brick in pairs(bricks.current_level_bricks) do
-        bricks.update_brick(dt, brick)
-    end
-end
-
-function bricks.update_brick(dt, brick)
-    
 end
 
 -- walls
 local walls = {thickness = 20}
-walls.current_level_walls = {}
 
 function walls.construct()
-    local screen_width = love.graphics.getWidth()
-    local screen_height = love.graphics.getHeight()
-    local top_left_corner = {x = 0, y = 0}
-    local top_right_corner = {x = screen_width - walls.thickness, y = 0}
-    local bottom_left_corner = {x = 0, y = screen_height - walls.thickness}
+    Objects.walls = {}
+    local top_center = {x = screen_width/2, y = walls.thickness/2}
+    local right_center = {x = screen_width - walls.thickness/2, y = screen_height/2}
+    local bottom_center = {x = screen_width/2, y = screen_height - walls.thickness/2}
+    local left_center = {x = walls.thickness/2, y = screen_height/2}
 
-    local top_wall = walls.add_wall("top", top_left_corner, screen_width, walls.thickness)
-    local left_wall = walls.add_wall("left", top_left_corner, walls.thickness, screen_height)
-    local right_wall = walls.add_wall("right", top_right_corner, walls.thickness, screen_height)
-    local bottom_wall = walls.add_wall("bottom", bottom_left_corner, screen_width, walls.thickness)
+    walls.add_wall("top", top_center, screen_width, walls.thickness)
+    walls.add_wall("right", right_center, walls.thickness, screen_height)
+    walls.add_wall("bottom", bottom_center, screen_width, walls.thickness)
+    walls.add_wall("left", left_center, walls.thickness, screen_height)
+
 end
 
-function walls.add_wall(direction, position, width, height)
-    local wall = {}
-    wall.position = position
-    wall.width = width
-    wall.height = height
-    walls.current_level_walls[direction] = wall
+function walls.add_wall(direction, center, width, height)
+    local wall_object = {
+        body = love.physics.newBody(World, center.x, center.y),
+        shape = love.physics.newRectangleShape(width, height)
+    }
+    wall_object.fixture = love.physics.newFixture(wall_object.body, wall_object.shape)
+    wall_object.fixture:setUserData({"Wall", direction})
+    Objects.walls[direction] = wall_object
 end
 
 function walls.draw()
-    for direction, wall in pairs(walls.current_level_walls) do
-        if direction ~= "bottom" then Draw_object_rectangle(wall) end
+    love.graphics.setColor(0.2, 0.2, 0.4)
+    for direction, wall in pairs(Objects.walls) do
+        love.graphics.polygon("fill", wall.body:getWorldPoints(wall.shape:getPoints()))
     end
-end
-
-function walls.update(dt)
-    for direction, wall in pairs(walls.current_level_walls) do
-        walls.update_wall(dt, wall)
-    end
-end
-
-function walls.update_wall(dt, wall)
 end
 
 -- ball
-local ball = {}
-ball.position = {x = 300, y = 300}
-ball.speed = {x = 300, y = 300}
-ball.radius = 10
-ball.segments = 16
-ball.hitbox = {width = 2 * ball.radius, height = 2 * ball.radius}
-ball.hitbox.position = {}
+local ball = {
+    speed = {x = 300, y = 300},
+    radius = 10,
+    segments = 16
+}
 
-function ball.update(dt)
-    ball.move(dt)
-
-end
-
-function ball.move(dt)
-    --TODO: normalize diagonal movement
-    ball.position.x = ball.position.x + ball.speed.x * dt
-    ball.position.y = ball.position.y + ball.speed.y * dt
-    ball.update_hitbox(dt)
-end
-
-function ball.update_hitbox(dt)
-    ball.hitbox.position = ball.position + Vector(-ball.radius, -ball.radius)
+function ball.build()
+    local ball_object = {}
+    ball_object.body = love.physics.newBody(World, love.graphics.getWidth()/2, 500, "dynamic")
+    ball_object.body:setInertia(0)
+    ball_object.shape = love.physics.newCircleShape(ball.radius)
+    ball_object.fixture = love.physics.newFixture(ball_object.body, ball_object.shape, 1)
+    ball_object.fixture:setRestitution(0.6)
+    ball_object.fixture:setUserData({"Ball", 1})
+    Objects.ball = ball_object
 end
 
 function ball.draw()
-    love.graphics.circle("line", ball.position.y, ball.position.y, ball.radius, ball.segments)
-    if debug == true then ball.draw_hitbox() end
-end
-
-function ball.draw_hitbox()
-    Draw_object_rectangle(ball.hitbox, {1, 0, 0})
+    local ball_object = Objects.ball
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.circle("line", ball_object.body:getX(), ball_object.body:getY(), ball_object.shape:getRadius(), ball.segments)
 end
 
 -- player
-local player = {}
-player.position = {x = 500, y = 500}
-player.speed = {x = 300, y = 0}
-player.width, player.height = 70, 20
-player.hitbox = {width = player.width - 2, height = player.height - 2}
-player.hitbox.position = {}
+local player = {
+    speed = {x = 500, y = 3},
+    width = 70,
+    height = 20
+}
 
-function player.update(dt)
-    player.move(dt)
+local mouse = {}
+
+function player.build()
+    local player_object = {}
+    player_object.body = love.physics.newBody(World, love.mouse.getX(), love.mouse.getY(), "dynamic")
+    player_object.body:setAngularDamping(0.1)
+    player_object.body:setFixedRotation(true)
+    player_object.shape = love.physics.newRectangleShape(player.width, player.height)
+    player_object.fixture = love.physics.newFixture(player_object.body, player_object.shape, 2)
+    player_object.fixture:setRestitution(0.5)
+    player_object.fixture:setUserData({"Player", 1})
+    player_object.joint = love.physics.newMouseJoint(player_object.body, love.mouse.getPosition())
+    Objects.player = player_object
 end
 
 function player.move(dt)
-    if love.keyboard.isDown("right") then
-        player.position.x = player.position.x + player.speed.x * dt
-    end
-    if love.keyboard.isDown("left") then
-        player.position.x = player.position.x - player.speed.x * dt
-    end
-    player.update_hitbox(dt)
-end
+    local player_object = Objects.player
+    local move_right = love.keyboard.isDown("right")
+    local move_left = love.keyboard.isDown("left")
+    local move_down = love.keyboard.isDown("down")
+    local move_up = love.keyboard.isDown("up")
 
-function player.update_hitbox(dt)
-    player.hitbox.position = player.position + Vector(1,1)
+    if move_right then
+        player_object.body:applyForce(player.speed.x, 0)
+    end
+    if move_left then
+        player_object.body:applyForce(-player.speed.x, 0)
+    end
+    if move_down then
+        player_object.body:applyLinearImpulse(0, player.speed.y)
+    end
+    if move_up then
+        player_object.body:applyLinearImpulse(0, -player.speed.y)
+    end
 end
 
 function player.draw()
-    Draw_object_rectangle(player)
-    if debug == true then player.draw_hitbox() end
-end
-
-function player.draw_hitbox()
-    Draw_object_rectangle(player.hitbox, {1, 0, 0})
-end
-
-
--- collisions
-local collisions = {}
-
-function collisions.resolve()
-    collisions.ball_player_collision(ball, player)
-    -- collisions.ball_walls_collision(ball, walls)
-    -- collisions.ball_bricks_collision(ball, bricks)
-    -- collisions.player_walls_collision(player, walls)
-end
-
-function collisions.rectangles_overlapping(a, b)
-    local overlap = false
-    local ax = a.position.x
-    local ay = a.position.y
-    local bx = b.position.x
-    local by = b.position.y
-    if not(ax + a.width < bx  or bx + b.width < ax or
-            ay + a.height < by or by + b.height < ay) then
-       overlap = true
-    end
-    return overlap
-end
-
-function collisions.ball_player_collision(ball, player)
-    if collisions.rectangles_overlapping(ball.hitbox, player.hitbox) then
-        print("ball-player collision")
-    end
+    local player_object = Objects.player
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.polygon("line", player_object.body:getWorldPoints(player_object.shape:getPoints()))
 end
 
 -- main loop
 love.graphics.setDefaultFilter('nearest', 'nearest')
 
+Objects = {}
+
 function love.load()
     Vector = require('lib.hump.vector')
     love.window.setTitle('My Arkanoid Clone')
+    local meter = 64
+    love.physics.setMeter(meter)
+    World = love.physics.newWorld(0, 9.81*meter, true)
+    World:setCallbacks(beginContact, endContact, preSolve, postSolve)
     bricks.construct_level()
     walls.construct()
-    debug = true
+    ball.build()
+    player.build()
 end
 
 function love.update(dt)
-    ball.update(dt)
-    player.update(dt)
-    bricks.update(dt)
-    walls.update(dt)
-
-    collisions.resolve()
+    World:update(dt)
+    Objects.player.joint:setTarget(love.mouse.getPosition())
+    -- player.move(dt)
 end
 
 function love.keypressed(key)
@@ -219,14 +191,33 @@ function love.draw()
     walls.draw()
 end
 
--- tools
+-- collisions callback
 
-function Draw_object_rectangle(object, color)
-    local mycolor = color or {1, 1, 1}
-    if object.position and object.width and object.height then
-        if mycolor then
-            love.graphics.setColor(mycolor)
-        end
-        love.graphics.rectangle("line", object.position.x, object.position.y, object.width, object.height)
+function beginContact(a, b, contact)
+    local dx, dy = contact:getNormal()
+    local aData = a:getUserData()
+	local bData = b:getUserData()
+    -- ball/bricks collisions
+    if aData[1] == "Ball" and bData[1] == "Brick" then
+        Bricks[bData[2]] = nil
+    elseif bData[1] == "Ball" and aData[1] == "Brick" then
+        Bricks[aData[2]].body:destroy()
+        Bricks[aData[2]] = nil
     end
+end
+
+function endContact(a, b, coll)
+    local textA = a:getUserData()
+	local textB = b:getUserData()
+
+end
+
+function preSolve(a, b, coll)
+    local textA = a:getUserData()
+    local textB = b:getUserData()
+
+end
+
+function postSolve(a, b, coll, normalimpulse, tangentimpulse)
+	
 end
